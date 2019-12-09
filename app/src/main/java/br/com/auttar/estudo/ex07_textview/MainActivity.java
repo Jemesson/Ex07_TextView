f212cec18f9eeb77afcf3e5132bffd54d9e2496d
Jemesson Lima <jemessonlima@gmail.com>
2019-12-08 21:58:23 -0300

    code improvement

--- app/src/main/java/br/com/auttar/estudo/ex07_textview/MainActivity.java
+++ app/src/main/java/br/com/auttar/estudo/ex07_textview/MainActivity.java
@@ -39,11 +39,11 @@ public class MainActivity extends Activity {
         final String html =
                 "<html>" +
                 "<body>" +
-                "<b>Negrito</b>, <i>Itálico</i>" +
+                "<b>Negrito</b>, <i>Itï¿½lico</i>" +
                 "e <u>Sublinhado</u>.<br>" +
                 "Mario: <img src='mario.png' /> <br>" +
                 "Yoshi: <img src='yoshi.png' /> <br>" +
-                "Um texto depois da imagem />" +
+                "Um textao depois da imagem />" +
                 "</body>" +
                 "</html>";
 
@@ -67,20 +67,15 @@ public class MainActivity extends Activity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (null == null && id == R.id.action_settings) {
             return true;
         }
 
